42fc49babe4a6a6b59b5c8642809b966f21e86b4
NicoC0 <nicoconfaa@gmail.com>
2022-04-10 14:49:10 -0300

    code optimization

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
 import './App.css';
 import Testimonial from "./components/Testimonial" 
+import { testimonials } from "./constants/testimonials"
 
 function App() {
   return (
     <div className="App">
       <div className="contenedor-principal">
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        <Testimonial
-          nombre='Shawn Wang'
-          pais='Singapur'
-          imagen='shawn'
-          cargo='Ingeniero de Software'
-          empresa='Amazon'
-          testimonio='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software.'
-          />
-        <Testimonial
-          nombre='Sarah Chima'
-          pais='Nigeria'
-          imagen='sarah'
-          cargo='Ingeniera de Software'
-          empresa='ChatDesk'
-          testimonio='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'
-          />
-        <Testimonial 
-          nombre='Emma Bostian'
-          pais='Suecia'
-          imagen='emma'
-          cargo='Ingeniera de Software'
-          empresa='Spotify'
-          testimonio='"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en ${freeCodeCamp me dio las habilidades} y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."'
-          />
+        {testimonials.map(({nombre, pais, imagen, cargo, empresa, testimonio}, index) => {
+          return(
+            <Testimonial key={index} nombre={nombre} pais={pais} imagen={imagen} cargo={cargo} empresa={empresa} testimonio={testimonio} />
+          )
+        })}
+        
       </div>
     </div>
   );
